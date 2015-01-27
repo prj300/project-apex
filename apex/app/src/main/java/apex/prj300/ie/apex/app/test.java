a21ea4dcb49c1b102895b39492f97c4249f75b5a
phelan1993 <s00126107@mail.itsligo.ie>
2015-01-27 21:27:04 +0000

    deleted test files

--- apex/app/src/main/java/apex/prj300/ie/apex/app/test.java
+++
@@ -1,7 +0,0 @@
-package apex.prj300.ie.apex.app;
-
-/**
- * Created by Phillip2 on 27/01/2015.
- */
-public class test {
-}
--- apex/app/src/main/java/test2.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Phillip2 on 27/01/2015.
- */
-public class test2 {
-}
